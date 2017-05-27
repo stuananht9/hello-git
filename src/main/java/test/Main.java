05fe60e37a838420c44f065275ccc63fb1980de4
tuananh <s.tuananht9@gmail.com>
2017-05-27 20:47:03 +0700

    Khoi tao project

---
+++ src/main/java/test/Main.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by tuananh on 05/27/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Commit 1");
+
+        System.out.println("Commit 2");
+    }
+}
